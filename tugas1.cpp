3768875c71041845174c572cdeb2468ae3583941
Ihsan020902 <albarrihsan@gmail.com>
2025-05-02 19:47:40 +0700

    Add files via upload

---
+++ tugas1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include<string.h>
+using namespace std;
+
+main(){
+	
+	int n;
+	cout<<"Masukkan bilangan : ";cin>>n;
+	if(n % 2 == 0){
+		cout<<"Adalah biangan genap"<<endl;
+	}else{
+	 	cout<<"adalah bilangan gnajil"<<endl;
+	}
+	return 0;
+}
